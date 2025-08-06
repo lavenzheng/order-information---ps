8fe52267d80ed0a96bbf8767e591ec9aa88e0c62
zl <zhzu1993@163.com>
2025-08-06 18:03:21 +0800

    v0.1

--- src/main.js
+++ src/main.js
@@ -16,26 +16,6 @@ entrypoints.setup({
           },
         });
       },
-    },
-    imageviewer: {
-      show(event) {
-        // 图片查看器面板直接加载imageviewer.html
-        // 这个面板会通过URL参数或postMessage接收图片URL
-        console.log('图片查看器面板已打开');
-        
-        // 设置面板内容为imageviewer.html
-        const container = document.getElementById('container');
-        if (container) {
-          // 创建iframe来加载图片查看器
-          const iframe = document.createElement('iframe');
-          iframe.src = './imageviewer.html';
-          iframe.style.width = '100%';
-          iframe.style.height = '100%';
-          iframe.style.border = 'none';
-          container.innerHTML = '';
-          container.appendChild(iframe);
-        }
-      },
-    },
+    }
   },
 });
